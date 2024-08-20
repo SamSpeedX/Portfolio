5c5d4286465d94c6a3855e1a4e41d176a42991b9
SAM OCHU <109700174+SamSpeedX@users.noreply.github.com>
2024-08-20 22:13:42 +0300

    Update home.php

--- home.php
+++ home.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
 
-if (!isset($_SESSION['user_id'])) {
-    header("location: logout.php");
+if (!isset($_SESSION['username'])) {
+    header("location: login.php");
     exit();
 }
 ?>
